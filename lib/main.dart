0cf31530cf0a1f6e20fa88ccc0e5ca689e0e1dca
ahmed <ahmedkarmous28@gmail.com>
2021-07-08 23:09:06 +0100

    setup localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_app_engineer/screens/splash_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +12,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Localizations Sample App',
+      localizationsDelegates: [
+        AppLocalizations.delegate, // Add this line
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''), // English, no country code
+        const Locale('es', ''), // Spanish, no country code
+      ],
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -17,6 +18,10 @@ class SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     // TODO: Implement build
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text(AppLocalizations.of(context)!.test_translate),
+      ),
+    );
   }
 }
