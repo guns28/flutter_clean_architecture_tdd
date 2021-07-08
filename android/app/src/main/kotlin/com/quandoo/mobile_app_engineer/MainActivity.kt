57fcada218c2234cdd4700594d08a5964f39582a
ahmed <ahmedkarmous28@gmail.com>
2021-07-08 22:43:28 +0100

    setup new project structure

---
+++ android/app/src/main/kotlin/com/quandoo/mobile_app_engineer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.quandoo.mobile_app_engineer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new SplashScreenState();
+  }
+}
+
+class SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Implement build
+    return Scaffold();
+  }
+}
